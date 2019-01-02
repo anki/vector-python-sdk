d9a6be8305e5e3816d4997f8dcc354de4ce0368c
Michelle Sintov <michelle@anki.com>
2019-01-02 10:09:31 -0800

    VIC-5166 Move say_text from robot to behavior (#82)

--- anki_vector/behavior.py
+++ anki_vector/behavior.py
@@ -176,6 +176,29 @@ class BehaviorComponent(util.Component):
         return await self.grpc_interface.DriveOnCharger(drive_on_charger_request)
 
     @connection.on_connection_thread()
+    async def say_text(self, text: str, use_vector_voice: bool = True, duration_scalar: float = 1.0) -> protocol.SayTextResponse:
+        """Make Vector speak text.
+
+        .. testcode::
+
+            import anki_vector
+            with anki_vector.Robot() as robot:
+                robot.behavior.say_text("Hello World")
+
+        :param text: The words for Vector to say.
+        :param use_vector_voice: Whether to use Vector's robot voice
+                (otherwise, he uses a generic human male voice).
+        :param duration_scalar: Adjust the relative duration of the
+                generated text to speech audio.
+
+        :return: object that provides the status and utterance state
+        """
+        say_text_request = protocol.SayTextRequest(text=text,
+                                                   use_vector_voice=use_vector_voice,
+                                                   duration_scalar=duration_scalar)
+        return await self.conn.grpc_interface.SayText(say_text_request)
+
+    @connection.on_connection_thread()
     async def set_eye_color(self, hue: float, saturation: float) -> protocol.SetEyeColorResponse:
         """Set Vector's eye color.
 
