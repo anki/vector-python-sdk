61872896e97ce96686a01ddeabcba533f12de604
Bruce von Kugelgen <45771625+BruceVonK@users.noreply.github.com>
2019-03-13 16:10:11 -0700

    VIC-13680 SDK plays audio on robot (#133)
    Python code for audio playback on Robot
    
    - runs with regular Robot, Async Robot
    - currently purposely limited to one audio stream at a time. This is a limitation of the current SDK Robot code, and changes would be dependent on Gateway changes for correctly routing response messages to the correct receivers (just like Remain Stationary Between Scripts VIC-12248).
    - Canceling an async audio playback future leaves everyone in a good state, but does not stop the stream in a timely fashion. This should be fixed with a Gateway change to add a "keep alive" to the audio stream like the Behavior Control stream.
    - Includes a tutorial, which includes 2 simple Anki wav files in a new 'sounds' folder
    - new pytest
    - Accidentally updated the victor_proto submodule, but branch was too old to be rebased easily.  Updated it to latest.

---
+++ examples/tutorials/11_play_audio.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2018 Anki, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the file LICENSE.txt or at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License isvi distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Play audio files through Vector's speaker.
+"""
+
+import anki_vector
+
+
+def main():
+    args = anki_vector.util.parse_command_args()
+    with anki_vector.Robot(args.serial) as robot:
+        robot.audio.stream_wav_file("sounds/vector_bell_whistle.wav", 75)
+        robot.audio.stream_wav_file("sounds/vector_alert.wav", 75)
+
+
+if __name__ == "__main__":
+    main()
